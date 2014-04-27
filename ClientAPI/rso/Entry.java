20ac1689437896d354faa6d31cf17dbd666bc943
Daniel Pogrebniak <daniel.pogrebniak@gmail.com>
2014-04-27 17:43:39 +0200

    dodanie metody toString

--- ClientAPI/rso/Entry.java
+++ ClientAPI/rso/Entry.java
@@ -59,5 +59,10 @@ public abstract class Entry {
     public void setName(String name) {
         this.name = name;
     }
+    
+    @Override
+    public String toString() {
+    	return getName();
+    }
 
 }
