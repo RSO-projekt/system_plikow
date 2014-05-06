4b35f223a4075ab862a5d015d1ca28402a2c1bb0
Daniel Pogrebniak <daniel.pogrebniak@gmail.com>
2014-05-06 21:44:42 +0200

    zmiana formatowania

--- ClientAPI/rso/FileSystemImpl.java
+++ ClientAPI/rso/FileSystemImpl.java
@@ -8,6 +8,7 @@ import rso.at.ClientMasterService;
 import rso.at.EntryNotFound;
 import rso.at.FileEntry;
 import rso.at.InvalidOperation;
+import rso.at.Transaction;
 
 public class FileSystemImpl implements FileSystem {
 
@@ -44,65 +45,77 @@ public class FileSystemImpl implements FileSystem {
 	}
 
 	@Override
-	public FileEntry makeDirectory(String path) throws EntryNotFound, InvalidOperation, TException {
+	public FileEntry makeDirectory(String path) throws EntryNotFound,
+			InvalidOperation, TException {
 		return service.makeDirectory(path);
 	}
 
 	@Override
-	public FileEntry makeDirectory(FileEntry parentDir, String name) throws EntryNotFound, InvalidOperation, TException {
+	public FileEntry makeDirectory(FileEntry parentDir, String name)
+			throws EntryNotFound, InvalidOperation, TException {
 		return service.makeDirectory2(parentDir, name);
 	}
 
 	@Override
-	public FileEntry makeFile(String path, long size) throws EntryNotFound, InvalidOperation, TException {
+	public FileEntry makeFile(String path, long size) throws EntryNotFound,
+			InvalidOperation, TException {
 		return service.makeFile(path, size);
 	}
 
 	@Override
-	public FileEntry makeFile(FileEntry parentDir, String name, long size) throws EntryNotFound, InvalidOperation, TException {
+	public FileEntry makeFile(FileEntry parentDir, String name, long size)
+			throws EntryNotFound, InvalidOperation, TException {
 		return service.makeFile2(parentDir, name, size);
 	}
 
 	@Override
-	public void removeEntry(String path) throws EntryNotFound, InvalidOperation, TException {
+	public void removeEntry(String path) throws EntryNotFound,
+			InvalidOperation, TException {
 		service.removeEntry(path);
 	}
 
 	@Override
-	public void removeEntry(FileEntry entry) throws EntryNotFound, InvalidOperation, TException {
+	public void removeEntry(FileEntry entry) throws EntryNotFound,
+			InvalidOperation, TException {
 		service.removeEntry2(entry);
 	}
 
 	@Override
-	public FileEntry moveEntry(String fromPath, String toPath) throws EntryNotFound, InvalidOperation, TException {
+	public FileEntry moveEntry(String fromPath, String toPath)
+			throws EntryNotFound, InvalidOperation, TException {
 		return service.moveEntry(fromPath, toPath);
 	}
 
 	@Override
-	public FileEntry moveEntry(FileEntry entry, FileEntry parentDir, String name) throws EntryNotFound, InvalidOperation, TException {
+	public FileEntry moveEntry(FileEntry entry, FileEntry parentDir, String name)
+			throws EntryNotFound, InvalidOperation, TException {
 		return service.moveEntry2(entry, parentDir, name);
 	}
 
 	@Override
-	public void writeToFile(String filePath, long offset, byte[] bytes) {
-//		service.writeToFile(filePath, offset, bytes); //TODO
+	public void writeToFile(String filePath, long offset, byte[] bytes)
+			throws EntryNotFound, InvalidOperation, TException {
+		// service.writeToFile(filePath, offset, bytes); //TODO
 	}
 
 	@Override
-	public void writeToFile(FileEntry file, long offset, byte[] bytes) {
-//		service.writeToFile(file, offset, bytes); //TODO
+	public void writeToFile(FileEntry file, long offset, byte[] bytes)
+			throws EntryNotFound, InvalidOperation, TException {
+		// service.writeToFile(file, offset, bytes); //TODO
 	}
 
 	@Override
-	public byte[] readFromFile(String filePath, long offset, long num) {
-//		return service.readFromFile(filePath, offset, num);
-		return null; //TODO
+	public byte[] readFromFile(String filePath, long offset, long num)
+			throws EntryNotFound, InvalidOperation, TException {
+		Transaction transaction = service.readFromFile(filePath, offset, num);
+		return null; // TODO
 	}
 
 	@Override
-	public byte[] readFromFile(FileEntry file, long offset, long num) {
-//		return service.readFromFile(file, offset, num);
-		return null; //TODO
+	public byte[] readFromFile(FileEntry file, long offset, long num)
+			throws EntryNotFound, InvalidOperation, TException {
+		Transaction transaction = service.readFromFile2(file, offset, num);
+		return null; // TODO
 	}
 
 }
