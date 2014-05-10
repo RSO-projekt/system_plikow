fd8afdcea5d514f695e3a9df5e64bd0bc02c2ceb
Przemysław Lenart <przemek.lenart@gmail.com>
2014-05-10 15:09:36 +0200

    Poprawki implementacyjne w serwerze głównym

--- src/impl/server/master/FileSystemMonitor.java
+++ src/impl/server/master/FileSystemMonitor.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-import org.apache.http.entity.FileEntity;
-
 import rso.at.FileEntry;
 import rso.at.FileEntryExtended;
 import rso.at.EntryNotFound;
@@ -19,60 +17,116 @@ public class FileSystemMonitor {
 	private TreeMap<Long, TreeSet<FileEntryExtended>> parentIdMap;
 	private Long nextId;
 	
+	private String pathParent;
+	private String pathName;
+		
+	
+	private FileEntryExtended createFileEntryExtended(FileType fileType, long time,
+													  long parentId, long size, String name)
+	{
+		FileEntry fe = new FileEntry(fileType, time, nextId, parentId, 0, size, name);
+		++nextId;
+		return new FileEntryExtended(fe, new ArrayList<Integer>(), FileState.IDLE);
+	}
+	
+	private void checkParentAndName(FileEntry parent, String name) throws InvalidOperation {
+		if (name.contains("/")) {
+			throw new InvalidOperation(9, "You cannot put '/' in directory name");
+		}
+		if (parent.type == FileType.FILE) {
+			throw new InvalidOperation(10, "Invalid path. You cannot create folder in a file");
+		}
+		
+		TreeSet<FileEntryExtended> childrenSet = parentIdMap.get(parent.id);
+		if (childrenSet == null) {
+			throw new InvalidOperation(19, "Parent doesn't exist: " + parent.name);
+		}
+		
+		boolean foundName = false;
+		for (FileEntryExtended fileEntryExtended : childrenSet) {
+			if (fileEntryExtended.entry.name.equals(name)) {
+				foundName = true;
+				break;
+			}
+		}
+		
+		if (foundName) {
+			throw new InvalidOperation(20, "File with specified name already exists");
+		}
+	}
+	
+	private void getParentPath(String path) throws EntryNotFound {
+		if (path.length() <= 1) {
+			throw new EntryNotFound(7, "Empty path not expected");
+		}
+		if (path.charAt(0) != '/') {
+			throw new EntryNotFound(8, "Path should start from root '/'");
+		}
+		if (path.charAt(path.length()-1) == '/') 
+			path = path.substring(0, path.length()-1);
+		
+		pathParent = path;
+		int lastSlash = path.lastIndexOf('/');
+		pathParent = path.substring(0, lastSlash);
+		if (pathParent.isEmpty()) pathParent = "/";
+		
+		pathName = path.substring(lastSlash + 1, path.length());
+	}
+	
 	public FileSystemMonitor() {
+		nextId = new Long(0);
 		idMap = new TreeMap<Long, FileEntryExtended>();
 		parentIdMap = new TreeMap<Long, TreeSet<FileEntryExtended>>();
-		FileEntry rootEntry = new FileEntry(FileType.DIRECTORY, 
-											System.currentTimeMillis()/1000,
-											0, 0, 0, 0, "root");
-		FileEntryExtended root = new FileEntryExtended(rootEntry, new ArrayList<Integer>(),
-				                                       FileState.IDLE);
+		FileEntryExtended root = createFileEntryExtended(FileType.DIRECTORY, 
+														 System.currentTimeMillis() / 1000,
+														 0, 0, "root");
 		idMap.put(0l, root);
 		TreeSet<FileEntryExtended> rootSet = new TreeSet<>();
 		parentIdMap.put(0l, rootSet);
-		nextId=1l;
 	}
 	
 	public synchronized FileEntryExtended getEntry(String path) throws EntryNotFound {
 		if (path.isEmpty()) {
-			throw new EntryNotFound(0, "Empty string not expected");
+			throw new EntryNotFound(0, "Empty path not expected");
 		}
 		String[] pathArray = path.split("/"); 
-		if (pathArray.length!=0 && !pathArray[0].isEmpty()) {
-			throw new EntryNotFound(0, "Path should start from root");
+		if (pathArray.length != 0 && !pathArray[0].isEmpty()) {
+			throw new EntryNotFound(1, "Path should start from root '/'");
 		}
-		FileEntryExtended fe=idMap.get(0l);
-		FileEntryExtended fe2=fe;
+		FileEntryExtended fe = idMap.get(0l);
+		FileEntryExtended fe2 = fe;
+		
 		for (int i=1; i<pathArray.length;++i) {
 			TreeSet<FileEntryExtended> dir = parentIdMap.get(fe.entry.id);
 			if (dir == null) {
-				throw new EntryNotFound(1, "Cannot find drectory children");
+				throw new EntryNotFound(2, "Cannot find directory's children: " + path);
 			}
 			for (FileEntryExtended fileEntryExtended : dir) {
 				if (fileEntryExtended.entry.name.equals(pathArray[i])) 
-					fe2=fileEntryExtended;
+					fe2 = fileEntryExtended;
 			}
-			if (fe==fe2)
-				throw new EntryNotFound(2, "Cannot find drectory children");
+			if (fe == fe2)
+				throw new EntryNotFound(3, "Cannot find drectory or file: " + path);
 			fe = fe2;
 		}
 		return fe.deepCopy();
 	}
 	
 	public synchronized List<FileEntry> lookup(String path, FileEntry parent) throws EntryNotFound, InvalidOperation {
-		FileEntryExtended entry=null;
+		FileEntryExtended entry = null;
 		if (!path.isEmpty()) 
 			entry = getEntry(path);	
-		else if (parent!=null)  
+		else if (parent != null)  
 			entry = idMap.get(parent.id);
-		if (entry==null)
-			throw new EntryNotFound(3, "Parent doesn't exist");
-		if (entry.entry.type==FileType.FILE)
-			throw new InvalidOperation(0, "File cannot has children");
+		if (entry == null)
+			throw new EntryNotFound(4, "Directory doesn't exist: " + parent.name );
+		if (entry.entry.type == FileType.FILE)
+			throw new InvalidOperation(5, "File cannot has children: " + entry.entry.name);
 		
 		TreeSet<FileEntryExtended> children = parentIdMap.get(entry.entry.id);
-		if (children==null)
-			throw new EntryNotFound(4, "Parent doesn't exist");
+		if (children == null)
+			throw new EntryNotFound(6, "[CRITICAL] Directory doesn't exist: " + entry.entry.name);
+		
 		List<FileEntry> childrenList = new ArrayList<FileEntry>();
 		for (FileEntryExtended fileEntry : children) {
 			childrenList.add(fileEntry.entry.deepCopy());
@@ -81,34 +135,16 @@ public class FileSystemMonitor {
 	}
 
 	public synchronized FileEntry makeDirectory(String path) throws EntryNotFound, InvalidOperation {
-		if (path.isEmpty()) {
-			throw new EntryNotFound(0, "Empty string not expected");
-		}
-		if (path.charAt(path.length()-1)=='/') 
-			path =path.substring(0, path.length()-1);
-		String parentPath = path;
-		if (path.lastIndexOf('/')!=0) {
-			parentPath = path.substring(0, path.lastIndexOf('/'));
-		}
-		else
-			parentPath="/";
-		String dirName = path.substring(path.lastIndexOf('/')+1, path.length());
-		FileEntryExtended parentDir = getEntry(parentPath);
-		return makeDirectory2(parentDir.entry, dirName);
+		getParentPath(path);
+		FileEntryExtended parentDir = getEntry(pathParent);
+		return makeDirectory2(parentDir.entry, pathName);
 	}
 	
 	public synchronized FileEntry makeDirectory2(FileEntry parent, String name) throws EntryNotFound, InvalidOperation {
-		if (name.contains("/"))
-			throw new InvalidOperation(2, "You cannot put / in directory name");
-		if (parent.type==FileType.FILE)
-			throw new InvalidOperation(1, "Invalid path. You cannot create folder in a file");
-		FileEntry dirEntry = new FileEntry(FileType.DIRECTORY, 
-				   System.currentTimeMillis()/1000,
-				   nextId, parent.id, 0, 0, name);
-		FileEntryExtended dir = new FileEntryExtended(dirEntry, new ArrayList<Integer>(),
-					                                  FileState.IDLE);
-		++nextId;
-		
+		checkParentAndName(parent, name);
+		FileEntryExtended dir = createFileEntryExtended(FileType.DIRECTORY, 
+				                                        System.currentTimeMillis() / 1000,
+				                                        parent.id, 0, name);
 		idMap.put(dir.entry.id, dir);
 		parentIdMap.put(dir.entry.id, new TreeSet<FileEntryExtended>());
 		parentIdMap.get(parent.id).add(dir);
@@ -116,28 +152,16 @@ public class FileSystemMonitor {
 	}
 	
 	public synchronized FileEntry makeFile(String path, long size) throws EntryNotFound, InvalidOperation {
-		if (path.isEmpty()) {
-			throw new EntryNotFound(0, "Empty string is not expected");
-		}
-		if (path.charAt(path.length()-1)=='/') 
-			path =path.substring(0, path.length()-1);
-		String parentPath = path.substring(0, path.lastIndexOf('/'));
-		String fileName = path.substring(path.lastIndexOf('/')+1, path.length());
-		FileEntryExtended parentEntry = getEntry(parentPath);
-		return makeFile2(parentEntry.entry, fileName, size);
+		getParentPath(path);
+		FileEntryExtended parentEntry = getEntry(pathParent);
+		return makeFile2(parentEntry.entry, pathName, size);
 	}
 	
 	public synchronized FileEntry makeFile2(FileEntry parent, String name, long size) throws EntryNotFound, InvalidOperation {
-		if (name.contains("/"))
-			throw new InvalidOperation(4, "You cannot put / in file name");
-		if (parent.type==FileType.FILE)
-			throw new InvalidOperation(3, "Invalid path. You cannot create file in a file");
-		FileEntry fileEntry = new FileEntry(FileType.FILE, 
-											System.currentTimeMillis()/1000,
-											nextId, parent.id, 0, size, name);
-		FileEntryExtended file = new FileEntryExtended(fileEntry, new ArrayList<Integer>(),
-							   						   FileState.IDLE);
-		++nextId;
+		checkParentAndName(parent, name);
+		FileEntryExtended file = createFileEntryExtended(FileType.FILE,
+														 System.currentTimeMillis() / 1000,
+														 parent.id, size, name);
 		
 		idMap.put(file.entry.id, file);
 		parentIdMap.get(parent.id).add(file);
@@ -145,67 +169,68 @@ public class FileSystemMonitor {
 	}
 	
 	public synchronized void removeEntry(String path) throws EntryNotFound, InvalidOperation {
-		if (path.isEmpty()) {
-			throw new EntryNotFound(0, "Empty string is not expected");
-		}
 		FileEntryExtended removingEntry = getEntry(path);
 		removeEntry2(removingEntry.entry);
 	}
 	
 	public synchronized void removeEntry2(FileEntry entry) throws EntryNotFound, InvalidOperation {
 		FileEntryExtended removingEntry = idMap.get(entry.id);
-		TreeSet<FileEntryExtended> parentTreeSet = parentIdMap.get(removingEntry.entry.id);
-		if (removingEntry.entry.type == FileType.DIRECTORY){
-			if(!parentTreeSet.isEmpty()){
-				throw new InvalidOperation(5, "Directory is not empty, you cannot remove unempty directory");
-			}
+		if (removingEntry == null) {
+			throw new EntryNotFound(13, "Entry for removal not found: " + entry.name);
 		}
-		parentTreeSet.remove(removingEntry);
+		if (entry.id == 0) {
+			throw new InvalidOperation(30, "Cannot remove root directory");
+		}
+		if (removingEntry.entry.type == FileType.DIRECTORY) {
+			TreeSet<FileEntryExtended> childrenSet = parentIdMap.get(removingEntry.entry.id);
+			if (!childrenSet.isEmpty()) {
+				throw new InvalidOperation(14, "You cannot remove unempty directory: " + entry.name);
+			}
+			parentIdMap.remove(removingEntry.entry.id);
+		}
+		
+		TreeSet<FileEntryExtended> childrenSet2 = parentIdMap.get(removingEntry.entry.parentID);
+		if (childrenSet2 == null) {
+			throw new InvalidOperation(15, "[CRITICAL] Entry doesn't have parent: " + entry.name);
+		}
+		
 		idMap.remove(removingEntry.entry.id);
+		childrenSet2.remove(removingEntry);
 	}
 	
 	public synchronized FileEntry moveEntry(String fromPath, String toPath) throws EntryNotFound, InvalidOperation {
-		FileEntryExtended from = getEntry(fromPath);
-		FileEntryExtended to;
-		try {
-			to = getEntry(toPath);
-			return moveEntry2(from.entry, to.entry, null);
-		} catch (Exception e) {
-			String parentPath = getParentPath(toPath);
-			to = getEntry(parentPath);
-			String name =  toPath.substring(toPath.lastIndexOf('/')+1, toPath.length());
-			return moveEntry2(from.entry, to.entry, name);
-		}
+		getParentPath(toPath);
+		FileEntryExtended parent = getEntry(pathParent);
+		FileEntryExtended entry = getEntry(fromPath);
+		return moveEntry2(entry.entry, parent.entry, pathName);
 	}
 	
 	public synchronized FileEntry moveEntry2(FileEntry entry, FileEntry parent, String name) throws EntryNotFound, InvalidOperation {
-		if (name.contains("/"))
-			throw new InvalidOperation(4, "You cannot put / in file name");
-		if (parent.type == FileType.FILE){
-			throw new InvalidOperation(6, "Place to move cannot be file");
+		checkParentAndName(parent, name);
+		FileEntryExtended entryExtended = idMap.get(entry.id);
+		if (entryExtended == null) {
+			throw new InvalidOperation(16, "Entry doesn't exist: " + entry.name);
 		}
-		TreeSet<FileEntryExtended> fromTree = parentIdMap.get(entry.parentID);
-		TreeSet<FileEntryExtended> toTree = parentIdMap.get(parent.id);
-		FileEntryExtended movingEntry = idMap.get(entry.id);
-		fromTree.remove(movingEntry);
-		toTree.add(movingEntry);
-		movingEntry.entry.parentID = parent.id;
-		if (!name.isEmpty()){
-			movingEntry.entry.name = name;
+		
+		if (entry.parentID == parent.id) {
+			entryExtended.entry.name = name;
+			return entryExtended.entry.deepCopy();
 		}
-		return movingEntry.entry.deepCopy();
-	}
-	
-	private String getParentPath(String path) {
-		if (path.charAt(path.length()-1)=='/') 
-			path =path.substring(0, path.length()-1);
-		String parentPath = path;
-		if (path.lastIndexOf('/')!=0) {
-			parentPath = path.substring(0, path.lastIndexOf('/'));
+		
+		TreeSet<FileEntryExtended> oldParentChildrenSet = parentIdMap.get(entry.parentID);
+		if (oldParentChildrenSet == null) {
+			throw new InvalidOperation(17, "Entry doesn't have parent: " + entry.name);
 		}
-		else
-			parentPath="/";
-		return parentPath;
+		oldParentChildrenSet.remove(entryExtended);
+		
+		TreeSet<FileEntryExtended> newParentChildrenSet = parentIdMap.get(parent.id);
+		if (newParentChildrenSet == null) {
+			throw new InvalidOperation(18, "New parent doesn't exist: " + entry.name);
+		}
+		newParentChildrenSet.add(entryExtended);
+		entryExtended.entry.parentID = parent.id;
+		entryExtended.entry.name = name;
+		return entryExtended.entry.deepCopy();
 	}
 }
 
