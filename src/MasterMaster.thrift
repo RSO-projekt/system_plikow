20c36b2f739d24a6c4b0275302647800d4621772
Przemysław Lenart <przemek.lenart@gmail.com>
2014-05-12 18:55:35 +0200

    Remove oneway z funkcji update

--- src/MasterMaster.thrift
+++ src/MasterMaster.thrift
@@ -9,16 +9,16 @@ namespace * rso.at
 service MasterMasterService
 {
     //----- SYNCHRONIZATION FUNCTIONS ------------------------------------------
-    oneway void updateCreateEntry(1: i32 serverID, 
-                                  2: i64 fsVersion, 
-                                  3: DataTypes.FileEntryExtended entry),
-    oneway void updateRemoveEntry(1: i32 serverID,
-                                  2: i64 fsVersion, 
-                                  3: DataTypes.FileEntryExtended entry),
-    oneway void updateMoveEntry(1: i32 serverID,
-                                2: i64 fsVersion, 
-                                3: DataTypes.FileEntryExtended oldEntry,
-                                4: DataTypes.FileEntryExtended newEntry),
+    void updateCreateEntry(1: i32 serverID, 
+                           2: i64 fsVersion, 
+                           3: DataTypes.FileEntryExtended entry),
+    void updateRemoveEntry(1: i32 serverID,
+                           2: i64 fsVersion, 
+                           3: DataTypes.FileEntryExtended entry),
+    void updateMoveEntry(1: i32 serverID,
+                         2: i64 fsVersion, 
+                         3: DataTypes.FileEntryExtended oldEntry,
+                         4: DataTypes.FileEntryExtended newEntry),
 
     // It can happen that server will be shutdown for a long time and it's
     // not possible to resync with master on delta basis.
