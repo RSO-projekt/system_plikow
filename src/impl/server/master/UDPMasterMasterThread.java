e0030cac583d26aa6298aabd08f2ef831575a186
Daniel Pogrebniak <daniel.pogrebniak@gmail.com>
2014-05-17 14:23:26 +0200

    DZIAŁA --> wersja na 2 etap

--- src/impl/server/master/UDPMasterMasterThread.java
+++ src/impl/server/master/UDPMasterMasterThread.java
@@ -3,6 +3,8 @@ package impl.server.master;
 import org.apache.thrift.TMultiplexedProcessor;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.server.TThreadPoolServer.Args;
 import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
@@ -13,7 +15,7 @@ import rso.at.MasterMasterService;
 class UDPMasterMasterThread extends Thread {
 	private FileSystemMonitor monitor;
 	private int serverID;
-	private TSimpleServer server;
+	private TThreadPoolServer server;
 	
 	public UDPMasterMasterThread(FileSystemMonitor monitor, int serverID) throws TTransportException{
 		this.monitor = monitor;
@@ -22,12 +24,12 @@ class UDPMasterMasterThread extends Thread {
 		TTransportFactory factory = new TFramedTransport.Factory();
 		
 		TMultiplexedProcessor processor = new TMultiplexedProcessor();
-		TServer.Args args = new TServer.Args(serverTransportExternal);
+		Args args = new TThreadPoolServer.Args(serverTransportExternal);
 		args.processor(processor);
 		args.transportFactory(factory);
 		
 		processor.registerProcessor("MasterMaster", new MasterMasterService.Processor<MasterMasterImpl>(new MasterMasterImpl(monitor)));
-		server = new TSimpleServer(args);
+		server = new TThreadPoolServer(args);
 		monitor.log("Starting UDP server on port " + Configuration.internalPort + 
 				    " with priority " + this.serverID + "...");
 	}
