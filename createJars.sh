02587644bc1f59509c9a659cb17dd6f23644f363
Daniel Pogrebniak <daniel.pogrebniak@gmail.com>
2014-06-07 20:30:13 +0200

    Optymalizacja tworzenia plików jar

--- createJars.sh
+++ createJars.sh
@@ -3,23 +3,22 @@ rm -r bin
 mkdir bin
 cp src/properties.conf .
 
+echo "Compiling java files"
+javac -d bin -nowarn -Xlint:unchecked -cp "lib/*" src/impl/*.java src/impl/client/*.java src/impl/server/master/*.java src/impl/server/data/*.java src/gen-java/rso/at/*.java 2> javas.log
+cd bin
+
 echo "Creating Client jar"
-javac -d bin -nowarn -Xlint:unchecked -cp "lib/*" src/impl/*.java src/impl/client/*.java src/gen-java/rso/at/*.java 2> javac.log
-jar cfm client.jar src/impl/client/MANIFEST.MF -C bin .
+jar cfm client.jar ../src/impl/client/MANIFEST.MF impl/Configuration.class rso/at/*.class impl/client/*.class
+mv client.jar ..
 
-rm -r bin
-mkdir bin
 echo "Creating MasterServer jar"
-javac -d bin -nowarn -Xlint:unchecked -cp "lib/*" src/impl/*.java src/impl/server/master/*.java src/gen-java/rso/at/*.java 2> javas.log
-jar cfm master-server.jar src/impl/server/master/MANIFEST.MF -C bin .
+jar cfm master-server.jar ../src/impl/server/master/MANIFEST.MF impl/Configuration.class rso/at/*.class impl/server/master/*.class 
+mv master-server.jar ..
 
-rm -r bin
-mkdir bin
 echo "Creating DataServer jar"
-javac -d bin -nowarn -Xlint:unchecked -cp "lib/*" src/impl/*.java src/impl/server/data/*.java src/gen-java/rso/at/*.java 2> javas.log
-jar cfm data-server.jar src/impl/server/data/MANIFEST.MF -C bin .
+jar cfm data-server.jar ../src/impl/server/data/MANIFEST.MF impl/Configuration.class rso/at/*.class impl/server/data/*.class 
+mv data-server.jar ..
 
-rm -r bin
-mkdir bin
+cd ..
 echo "Done"
 
